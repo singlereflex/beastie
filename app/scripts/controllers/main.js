d88b20f75f8d65d20a39458fb57d8e3162f9a2ed
spaceribs <spaceribs@gmail.com>
2013-11-11 12:39:36 -0500

    Init

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,7 @@ angular.module('beastieApp')
         $scope.iconPrefix = 'icon-';
 
         for (var i = 0; i < gridsize; i++) {
-            backgrid[i] = Array(gridsize);
+            backgrid[i] = new Array(gridsize);
             for (var e = 0; e < gridsize; e++) {
                 var blocktype = _.sample(beastieEnv);
                 var classVal = '';
@@ -41,9 +41,68 @@ angular.module('beastieApp')
         $scope.entities = [
             {
                 kind: 'player',
+                classVal: $scope.iconPrefix + 'entities_player',
                 keyboard: true,
-                x: gridsize/2,
-                y: gridsize/2
+                x: Math.floor(Math.random()*gridsize),
+                y: Math.floor(Math.random()*gridsize)
             }
         ];
+
+        for (var i = 0; i < 10; i++) {
+            $scope.entities.push({
+                kind: 'monster',
+                classVal: $scope.iconPrefix + 'entities_monster',
+                keyboard: false,
+                x: Math.floor(Math.random()*gridsize),
+                y: Math.floor(Math.random()*gridsize)
+            });
+        }
+
+        for (var i = 0; i < 5; i++) {
+            $scope.entities.push({
+                kind: 'mother',
+                classVal: $scope.iconPrefix + 'entities_mother',
+                keyboard: false,
+                x: Math.floor(Math.random()*gridsize),
+                y: Math.floor(Math.random()*gridsize)
+            });
+        }
+
+        for (var i = 0; i < 20; i++) {
+            $scope.entities.push({
+                kind: 'egg',
+                classVal: $scope.iconPrefix + 'entities_egg',
+                keyboard: false,
+                x: Math.floor(Math.random()*gridsize),
+                y: Math.floor(Math.random()*gridsize)
+            });
+        }
+
+        var frame = 0;
+
+        (function animloop(){
+            frame++;
+
+            requestAnimFrame(animloop);
+//            if (frame % 1 === 0) {
+//                angular.forEach($scope.entities, function(value, key){
+//                    $scope.$apply(function(){
+//                        if (value.kind === "monster") {
+//                            var newX = value.x + (Math.floor(Math.random() * 3) - 1);
+//                            var newY = value.y + (Math.floor(Math.random() * 3) - 1);
+//
+//                            newX = Math.max(0, newX);
+//                            newX = Math.min(gridsize, newX);
+//
+//                            newY = Math.max(0, newY);
+//                            newY = Math.min(gridsize, newY);
+//
+//                            value.x = Math.floor(newX);
+//                            value.y = Math.floor(newY);
+//                        }
+//                    });
+//                });
+//            }
+        })();
+
     }]);
