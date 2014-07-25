11c3eee98daa4711b80a2509ab2b1a764be34ddb
David Gage <whytheplatypus@gmail.com>
2014-07-25 09:28:50 -0400

    watch tasks for scss and svg fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-var iconfont = require('gulp-iconfont');
 
+var iconfont = require('gulp-iconfont');
 gulp.task('iconfont', function(){
   gulp.src(['app/svg/*.svg'])
     .pipe(iconfont({
@@ -15,9 +15,27 @@ gulp.task('iconfont', function(){
 });
 
 var sass = require('gulp-sass')
-
 gulp.task('sass', function () {
     gulp.src('app/styles/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/styles'));
+});
+
+var livereload = require('gulp-livereload'),
+    dest = 'app';
+
+gulp.task('server', function(next) {
+  var connect = require('connect'),
+      server = connect();
+  var serveStatic = require('serve-static');
+  server.use(serveStatic(dest)).listen(process.env.PORT || 8080, next);
+});
+
+gulp.task('watch', ['server'], function() {
+  var server = livereload();
+  gulp.watch(dest + '/**').on('change', function(file) {
+      server.changed(file.path);
+  });
+  gulp.watch(dest + '/styles/*.scss', ['sass']);
+  gulp.watch(dest + '/svg', ['iconfont']);
 });
