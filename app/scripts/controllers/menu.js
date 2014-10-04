1944b5515ef8833a9037054359d978042ec122a1
David Gage <whytheplatypus@gmail.com>
2014-10-04 16:00:37 -0400

    tell the world about the manifest

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -9,6 +9,60 @@
  */
 angular.module("beastieApp")
     .controller("MenuCtrl", function ($scope) {
+        $scope.appCache = window.applicationCache;
+
+        $scope.appCache.update();
+        function handleCacheEvent(e){
+          if(!$scope.$$phase){
+            $scope.$apply();
+          }
+          console.debug(e);
+        }
+        // Fired after the first cache of the manifest.
+        $scope.appCache.addEventListener('cached', handleCacheEvent, false);
+
+        // Checking for an update. Always the first event fired in the sequence.
+        $scope.appCache.addEventListener('checking', function(e){
+          if($scope.update_state != "updated (reload to see)"){
+            $scope.update_state = "looking for update...";
+          }
+          handleCacheEvent(e);
+        }, false);
+
+        // An update was found. The browser is fetching resources.
+        $scope.appCache.addEventListener('downloading', function(e){
+          $scope.update_state = "updating...";
+          handleCacheEvent(e);
+        }, false);
+
+        // The manifest returns 404 or 410, the download failed,
+        // or the manifest changed while the download was in progress.
+        $scope.appCache.addEventListener('error', handleCacheEvent, false);
+
+        // Fired after the first download of the manifest.
+        $scope.appCache.addEventListener('noupdate', function(e){
+          if($scope.update_state != "updated (reload to see)"){
+            $scope.update_state = "up to date";
+          }
+          handleCacheEvent(e);
+        }, false);
+
+        // Fired if the manifest file returns a 404 or 410.
+        // This results in the application cache being deleted.
+        $scope.appCache.addEventListener('obsolete', handleCacheEvent, false);
+
+        // Fired for each resource listed in the manifest as it is being fetched.
+        $scope.appCache.addEventListener('progress', handleCacheEvent, false);
+
+        // Fired when the manifest resources have been newly redownloaded.
+        $scope.appCache.addEventListener('updateready', function(e){
+          $scope.update_state = "updated (reload to see)";
+          handleCacheEvent(e);
+          $scope.appCache.swapCache();
+
+
+        }, false);
+
         $scope.entities = [];
 
         var letters = [
